536dfd4afc4cbf6a0eaa860fa307ab8994aa9d5c
carlabastosdev <carlananci@hotmail.com>
2021-11-15 03:02:19 -0300

    construindo JS

--- index.js
+++ index.js
@@ -25,3 +25,42 @@ function inserirAtividade() {
     salvarLocalStorage(novaLista);
     carregarListaAtividades(novaLista);
 }
+function carregarListaAtividades(listaItens) {
+    const lista = document.getElementById("toDoList");
+
+    lista.innerHTML = " ";
+
+    listaItens.map((item, index) => {
+        const atividade = document.createElement('div');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.addEventListener("change", event => selecionarAtividade(index, event.target.checked));
+
+        const span = document.createElement('span');
+        span.id = `span${index}`;
+        span.innerHTML = item.descricao;
+
+        if (item.feito) {
+            checkbox.checked = true;
+            span.style.textDecoration = "line-through";
+        }
+
+        const button = document.createElement('button');
+        button.innerHTML = 'X';
+        button.addEventListener("click", () => deleteAtividade(index));
+
+        atividade.appendChild(checkbox);
+        atividade.appendChild(span);
+        atividade.appendChild(button);
+
+        lista.appendChild(atividade);
+    })
+}
+
+function Inicio() {
+    const itens = carregarLocalStorage();
+    carregarListaAtividades(itens);
+}
+
+Inicio();
